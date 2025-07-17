ea000066f908c209de859cf3f21272dee08e609f
Amirbek Kadirov <119916186+DoomSoviet@users.noreply.github.com>
2025-07-17 13:39:06 +0530

    Added GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,10 +59,24 @@ export default function RootLayout({
         <meta name="author" content="Odyssey Sri Lanka" />
         <link rel="icon" href="/Logo/odyssey-logo.jpeg" type="image/x-icon" />
         <link rel="apple-touch-icon" href="/Logo/odyssey-logo.jpeg" />
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{
+          __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-KGPV268T');
+        ` }} />
+        {/* End Google Tag Manager */}
       </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KGPV268T"
+            height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
       </body>
     </html>
